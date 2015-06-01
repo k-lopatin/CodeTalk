9034fb471f541cc6f7af3cef73d43ed34f799714
Rostislav <shtanko243@gmail.com>
2015-06-01 12:54:27 +0300

    Chat generating button

--- controllers/project.php
+++ controllers/project.php
@@ -3,9 +3,9 @@
 class ProjectController extends Controller {
 	
 	function index( $f = '' ){	
-		$projectHelper = $this->getHelper('project');
-		echo $projectHelper::createId();
-
+		//$projectHelper = $this->getHelper('project');
+		//echo $projectHelper::createId();
+		echo $f;
 		$this->showView('chat', $this->vars);
 	} 
 
